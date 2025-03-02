370755ef29afdc47a6e90d738c9eb68797aec931
Muhammad Syahril Faruq <mrfaruq2001@gmail.com>
2025-03-02 19:58:01 +0700

    feat: add .gitignore for view cache and update .vercelignore to exclude storage; remove unused view files

--- bootstrap/cache/routes-v7.php
+++ bootstrap/cache/routes-v7.php
@@ -33,7 +33,7 @@ app('router')->setCompiledRoutes(
         array (
           0 => 
           array (
-            '_route' => 'generated::p5FE5SmVyQpO3D8e',
+            '_route' => 'generated::vSUf7XFvRUp1Pzsb',
           ),
           1 => NULL,
           2 => 
@@ -1739,7 +1739,7 @@ app('router')->setCompiledRoutes(
       'waitSeconds' => NULL,
       'withTrashed' => false,
     ),
-    'generated::p5FE5SmVyQpO3D8e' => 
+    'generated::vSUf7XFvRUp1Pzsb' => 
     array (
       'methods' => 
       array (
@@ -1768,7 +1768,7 @@ app('router')->setCompiledRoutes(
                         \'exception\' => $exception,
                     ]), status: $exception ? 500 : 200);
                 }";s:5:"scope";s:54:"Illuminate\\Foundation\\Configuration\\ApplicationBuilder";s:4:"this";N;s:4:"self";s:32:"000000000000071a0000000000000000";}}',
-        'as' => 'generated::p5FE5SmVyQpO3D8e',
+        'as' => 'generated::vSUf7XFvRUp1Pzsb',
       ),
       'fallback' => false,
       'defaults' => 

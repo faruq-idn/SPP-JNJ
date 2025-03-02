a91279a2b491030c422bd573c92558cc3b8e2436
Muhammad Syahril Faruq <mrfaruq2001@gmail.com>
2025-03-02 18:40:29 +0700

    feat: comment out debugging output in index.php; streamline environment testing

--- api/index.php
+++ api/index.php
@@ -1,37 +1,38 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+require __DIR__ . '/../public/index.php';
+// error_reporting(E_ALL);
+// ini_set('display_errors', '1');
 
-echo <<<HTML
-<pre>
-PHP Environment Test
--------------------
-PHP Version: " . PHP_VERSION . "
-Time: " . date('Y-m-d H:i:s') . "
-Server: " . php_uname() . "
+// echo <<<HTML
+// <pre>
+// PHP Environment Test
+// -------------------
+// PHP Version: " . PHP_VERSION . "
+// Time: " . date('Y-m-d H:i:s') . "
+// Server: " . php_uname() . "
 
-Environment Variables:
---------------------
-VERCEL_URL: {$_ENV['VERCEL_URL']}
-APP_ENV: {$_ENV['APP_ENV']}
+// Environment Variables:
+// --------------------
+// VERCEL_URL: {$_ENV['VERCEL_URL']}
+// APP_ENV: {$_ENV['APP_ENV']}
 
-Extensions:
-----------
-HTML;
+// Extensions:
+// ----------
+// HTML;
 
-echo implode("\n", get_loaded_extensions());
+// echo implode("\n", get_loaded_extensions());
 
-echo "\n\nDirectory Test:\n";
-echo "-------------\n";
-$testDir = '/tmp/test-' . time();
-if (mkdir($testDir, 0777, true)) {
-    echo "Successfully created test directory\n";
-    if (is_writable($testDir)) {
-        echo "Directory is writable\n";
-    }
-    rmdir($testDir);
-} else {
-    echo "Failed to create directory\n";
-}
+// echo "\n\nDirectory Test:\n";
+// echo "-------------\n";
+// $testDir = '/tmp/test-' . time();
+// if (mkdir($testDir, 0777, true)) {
+//     echo "Successfully created test directory\n";
+//     if (is_writable($testDir)) {
+//         echo "Directory is writable\n";
+//     }
+//     rmdir($testDir);
+// } else {
+//     echo "Failed to create directory\n";
+// }
 
-echo "</pre>";
+// echo "</pre>";
